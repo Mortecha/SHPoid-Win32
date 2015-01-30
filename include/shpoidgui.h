bc352688f44bf3c23e855bc3b5d8c2f1f70ae460
Mortecha <sam.keightley@gmail.com>
2015-01-31 01:31:53 +1000

    further cleanup before toolbar abst

--- include/shpoidgui.h
+++ include/shpoidgui.h
@@ -11,25 +11,6 @@ class SHPoidGUI
         SHPoidGUI();
         virtual ~SHPoidGUI();
         void Create(HWND hwnd, HINSTANCE hInstance);
-        HWND GetMainToolbarHandle()
-        {
-            return m_hMainToolbar;
-        }
-
-        HWND GetDrawControlsToolbarHandle()
-        {
-            return m_hDrawToolbar;
-        }
-
-    protected:
-    private:
-        HWND m_hMainToolbar;
-        HWND m_hDrawToolbar;
-
-        void AddDrawButtons(HWND hToolbar, HINSTANCE hInstance);
-        void AddMainButtons(HWND hToolbar, HINSTANCE hInstance);
-        void CreateToolbar(HWND hwnd, HINSTANCE hInstance);
-        void CreateDrawToolbar(HWND hwnd, HINSTANCE hInstance);
 };
 
 #endif // SHPOIDGUI_H
