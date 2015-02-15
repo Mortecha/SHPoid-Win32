0d5dc60c273e6735003a218476580be86817b079
Sam Keightley <sam.keightley@gmail.com>
2015-02-16 03:53:50 +1000

    Populated filter settings dialog box

--- include/shpoidgui.h
+++ include/shpoidgui.h
@@ -4,7 +4,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <commctrl.h>
-#include "components/mainrebar.h"
 #include "components/toolrebar.h"
 #include "components/maintoolbar.h"
 #include "components/toolstoolbar.h"
@@ -21,7 +20,6 @@ class SHPoidGUI
         HWND GetToolsToolbarHandle();
 
     private:
-        MainRebar m_mainRebar;
         ToolRebar m_toolRebar;
         MainToolbar m_mainToolbar;
         ToolsToolbar m_toolsToolbar;
